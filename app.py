de42c390b89b39493c0cdef27f5368d9a2837f79
Zoro-Chan <87255924+Zoro-Chan@users.noreply.github.com>
2021-08-22 12:14:21 +0800

    Run the package

--- app.py
+++ app.py
@@ -1,32 +1,5 @@
-from flask import *
-
-app = Flask(__name__)
-
-topics = [
-    {
-        "name":"q/questions",
-    },
-
-    {
-        "name":"r/reality",
-    },
-
-    {
-        "name":"m/magic",
-    },
-]
-
-@app.route('/')
-def main():
-    return render_template('index.html', topics = topics)
-
-@app.route('/home/<name>')
-def home(name):
-    return "<h1>Welcome Back " + name + "</h1>"
-
-@app.route('/<bio>/<name>')
-def bio(bio, name):
-    return render_template('bio.html', bio = bio, name = name)
+from FlaskSite import app
+from random import randint
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=randint(2000, 9000))
