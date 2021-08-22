0f740325ea30f53b69071e43d5d830abb1b19856
Zoro-Chan <87255924+Zoro-Chan@users.noreply.github.com>
2021-08-22 19:05:52 +0800

    package's main script

--- FlaskSite/__init__.py
+++ FlaskSite/__init__.py
@@ -6,7 +6,7 @@ import random
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'KEY--afe0aa86371e5d84c1d5bb132bc82d1865869b0c5--KEY'
+app.config['SECRET_KEY'] = 'KEY--[REDACTED]--KEY'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
